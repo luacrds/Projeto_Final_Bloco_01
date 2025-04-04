9845edbd2eeaea00b08b2972894bbea97f413d82
unknown <luanacardosos@outlook.com>
2025-04-04 12:58:30 -0300

    Construção das Classes Model e Interface Repository

---
+++ projeto_final_bloco_01/src/model/Ingresso.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Ingresso extends Produto {
+
+    public Ingresso(int id, String nome, double preco, int quantidade) {
+        super(id, nome, preco, quantidade);
+    }
+
+    @Override
+    public void visualizar() {
+        super.visualizar();
+    }
+}
